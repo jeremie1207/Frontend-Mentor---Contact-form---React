4f4a9268c8b86a84744577bd71b62ece4c929502
Jeremie Barhabazi <71102646+jeremie1207@users.noreply.github.com>
2026-02-20 12:53:14 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,8 @@ import { useState } from "react";
 import "./App.css";
 import ContactForm from "./Component/ContactForm/ContactForm";
 import { Footer } from "./Component/Footer/Footer";
-import successCheck from "/assets/images/icon-success-check.svg";
+import SuccessMessageToast from "./Component/SuccessMessageToast/SuccessMessageToast";
+
 
 function App() {
 
@@ -19,15 +20,7 @@ function App() {
   return (
     <>
       <main>
-        <section role="alert" className="success-message__container" data-show={successStatus}>
-        <div className="success-message">
-          <div className="success-message__title-container">
-            <img src={successCheck} alt="" />
-            <h2 className="success_message-title">Message Sent!</h2>
-          </div>
-         <p>Thanks for completing the form. We’ll be in touch soon!</p>
-        </div>
-      </section>
+        <SuccessMessageToast onSuccess={successStatus} />
         <ContactForm onSuccess={handleSuccess}/>
       </main>
       <Footer />
@@ -36,4 +29,3 @@ function App() {
 }
 
 export default App;
-
